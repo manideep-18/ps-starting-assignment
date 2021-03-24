5f7ce4fd7fc3f74a86137870bd557513e5569188
manideep-18 <r8manideep@gmail.com>
2021-03-24 18:44:11 +0530

    sass and react-router-dom packages installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import { returnStatement } from "@babel/types";
+import Routes from "./Routes";
 
 class App extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div className="responsiveContainer">
+        <Routes />
+      </div>
+    );
   }
 }
 
